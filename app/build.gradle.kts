6b8c87bd2e9cb696681d6e65a9f8502ec6e6e07b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-06-28 18:56:20 -0500

    bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,8 +121,8 @@ android {
         applicationId = "online.fujinet.go.msx"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4 
-        versionName = "0.6.0"
+        versionCode = 5 
+        versionName = "0.6.1"
         buildConfigField("String", "OPENMSX_VERSION", "\"${openMsxVersion}\"")
         buildConfigField("String", "FUJINET_RUNTIME_VERSION", "\"${fujiNetRuntimeVersion}\"")
 
